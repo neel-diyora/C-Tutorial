b6a10d0fe36e3ff0152acac486e025ee9bc452a9
neel-diyora <neeldiyora5@gmail.com>
2021-06-16 22:55:17 +0530

    second commit

--- tut24.cpp
+++ tut24.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // call by reference using C++ reference variables
 void swapReferenceVar(int &a, int &b){ //temp  a  b
     int temp = a;                // 4    4  5
-    a = b;                      // 4    5  5
+    a = b;                       // 4    5  5
     b = temp;                    // 4    5  4
 }
 int main(){
